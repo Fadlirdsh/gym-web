ffa2f30fb9482dad23998589e996dff8ff4761ac
Fadlirdsh <if23.mochamadrudiansyah@mhs.ubpkarawang.ac.id>
2025-09-16 22:58:26 +0700

    FE login di admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('users.home');
 });
+
+// Semua fitur yang harus login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//     Route::get('/fitur1', [FiturController::class, 'fitur1'])->name('fitur1');
+//     Route::get('/fitur2', [FiturController::class, 'fitur2'])->name('fitur2');
+// });
+
+// Route::post('/logout', function () {
+//     Auth::logout();
+//     return redirect('/'); // arahkan ke halaman login atau home
+// })->name('logout');
